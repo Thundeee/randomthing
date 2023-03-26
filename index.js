58cf764040b6fbc100dd06de3ad81d296d3354b1
Asbjørn <78221203+Thundeee@users.noreply.github.com>
2023-03-26 20:20:58 +0200

    done

---
+++ index.js
@@ -0,0 +1,42 @@
+let playerArray;
+
+let form = document.querySelector('.playerForm');
+let team1 = document.querySelector('.team1');
+let team2 = document.querySelector('.team2');
+
+console.log(form);
+form.addEventListener('submit', function(e) {
+ playerArray = [];
+  e.preventDefault();
+  for (let i = 0; i < form.length -1; i++) {
+    if (form[i].value === '') {
+        continue;
+        
+    }
+        
+    playerArray.push(form[i].value);
+  }
+  console.log(playerArray);
+
+  //randomize the array
+    for (let i = playerArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [playerArray[i], playerArray[j]] = [playerArray[j], playerArray[i]];
+
+    }
+
+    for (let i = 0; i < playerArray.length; i++) {
+        let playerElement = document.createElement('li');
+        playerElement.textContent = playerArray[i];
+        playerElement.classList.add('player');
+        if (i % 2 === 0) {
+            team1.appendChild(playerElement);
+
+
+        } else {
+            team2.appendChild(playerElement);
+            
+        }
+    }
+  
+});
